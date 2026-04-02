bddbe9757be4de29d597d448f38fc7ae2d7fc037
rogovogor17 <tolik_rogov@bk.ru>
2026-04-03 02:30:41 +0300

    done test for cpp

---
+++ Cpp/tests/test_hard.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+
+int mul(int a, int b) {
+    return a * b;
+}
+
+class MulTest : public ::testing::TestWithParam<std::tuple<int, int, int>> {};
+
+TEST_P(MulTest, AddParameters) {
+    auto [a, b, expected] = GetParam();
+    EXPECT_EQ(mul(a, b), expected);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    MulTests,
+    MulTest,
+    ::testing::Values(
+        std::make_tuple(1, 1, 1),
+        std::make_tuple(2, 3, 6),
+        std::make_tuple(-1, 1, -1)
+    )
+);
---
+++ Cpp/tests/test_simple.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+class MathTest : public ::testing::TestWithParam<std::tuple<int, int, int>> {};
+
+TEST_P(MathTest, AddParameters) {
+    auto [a, b, expected] = GetParam();
+    EXPECT_EQ(add(a, b), expected);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    AddTests,
+    MathTest,
+    ::testing::Values(
+        std::make_tuple(1, 1, 2),
+        std::make_tuple(2, 3, 5),
+        std::make_tuple(-1, 1, 0)
+    )
+);
